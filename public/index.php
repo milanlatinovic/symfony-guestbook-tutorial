fa168a99c5440201ce84401df917c85b01258560
Milan Latinovic <milan.softline@gmail.com>
2020-11-20 22:27:16 +0100

    Dump commented out

--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@ require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
-
     Debug::enable();
 }
 
@@ -23,6 +22,7 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+// dump($request);
 $response->send();
 
 $kernel->terminate($request, $response);
